eab33fac2b0c2fb01fabc544fa6c0c61c5f1255a
Brendan Munnelly <munnellyb@gmail.com>
2021-11-10 17:18:08 +0000

    Add files via upload

---
+++ lunadoge/assets/js/footer.js
@@ -0,0 +1,74 @@
+/*
+//////////////// MENUS AND DROPDOWNS ///////////////
+*/
+
+// Deselect all
+
+let divs = document.querySelectorAll('.dropbtn');
+divs.forEach(el => el.addEventListener('click', event => {
+    // hide all
+    let i;
+    for (i = 1; i < 5; i++) {
+        document.querySelector("#content-"+i).classList.add("dropdown-hidden");
+    }
+    // get id
+    const str = event.target.id.toString();
+    const id = str.charAt(str.length-1);
+    const elItem_show = document.querySelector("#content-"+id);
+    elItem_show.classList.remove("dropdown-hidden"); 
+}));
+
+// Hide menus when click on page
+document.querySelector("#HTML-Content").addEventListener("click", hideMenus);
+
+function hideMenus() {
+    let i;
+    for (i = 1; i < 5; i++) {
+        document.querySelector("#content-"+i).classList.add("dropdown-hidden");
+    }
+}
+
+
+/*
+//////////////// COPY TO CLIPBOARD ///////////////
+*/
+
+document.querySelector("#btn-copy").addEventListener("click", copyHTML);
+
+function copyHTML() {
+    // removeEmptyNodes();
+    let HTML_Content = document.getElementById("HTML-Content").innerHTML;
+    const el = document.createElement('textarea');
+    el.value = HTML_Content;
+    hideMenus();
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
+
+
+/*
+//////////////// REMOVE LEFTOVER NODES  ///////////////
+*/
+
+function removeEmptyNodes() {
+    const HTML_Content = document.querySelector("#HTML-Content");
+    var treeWalker = document.createTreeWalker(HTML_Content, NodeFilter.SHOW_ELEMENT);
+    var currentNode = treeWalker.currentNode
+    var emptyNodes = []
+
+    // test if a node has no text, regardless of whitespaces
+    var isNodeEmpty = node => !node.textContent.trim()
+
+    // find all empty nodes
+    while(currentNode) {
+        isNodeEmpty(currentNode) && emptyNodes.push(currentNode)
+        currentNode = treeWalker.nextNode()
+    }
+
+    // remove found empty nodes
+    emptyNodes.forEach(node => node.parentNode.removeChild(node))
+    return;
+}
+
