693507d549c2de934fc6a010ab5b346d5ba9750e
Brendan Munnelly <munnellyb@gmail.com>
2022-03-14 06:33:10 +0000

    Add files via upload

--- lunevedy/assets/js/grid-general.js
+++ lunevedy/assets/js/grid-general.js
@@ -8,6 +8,17 @@ let section_class ="section-selector-1";
 // Used by color picker. Either 'theme-light' or 'theme-dark'.
 let section_theme = "section.theme-light."+section_class;
 
+// GLOBAL VARIABLES
+
+// Class names of columns in grid: .col-2, .col-3 or .col-4
+let col_no;
+if (document.querySelector('.col-2')) { col_no = ".col-2" }
+else if (document.querySelector('.col-3')) { col_no = ".col-3" }
+else if (document.querySelector('.col-4')) { col_no = ".col-4" }
+
+// Number column blocks to loop through
+let col_count = document.querySelectorAll(col_no).length;
+
 /*
 //////////////// MENUS AND DROPDOWNS ///////////////
 */
