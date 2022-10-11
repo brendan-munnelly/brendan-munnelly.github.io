d41c6ed1ca10ca5f17851fae1737e836d2d94f9d
Brendan Munnelly <munnellyb@gmail.com>
2022-10-11 16:41:50 +0100

    Add files via upload

---
+++ reactjs/exercises/2/final.js
@@ -0,0 +1,31 @@
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// const header = ReactDOM.createRoot(document.getElementById("header"))
+// const main = ReactDOM.createRoot(document.getElementById("main"))
+
+// header.render(<header><h1>Corporate strategy</h1></header>)
+// root.render(<main><section><p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p></section></main>)
+
+
+// ReactDOM.render(
+//     <HeaderContent />,
+//     document.getElementById("header")
+// )
+
+// ReactDOM.render(
+//     <MainContent />,
+//     document.getElementById("main")
+// )
+
+// function HeaderContent () {
+//     return (
+//        <header><h1>Corporate strategy</h1></header>
+//     )
+// }
+
+// function MainContent () {
+//     return (
+//        <main><section><p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p></section></main>
+//     )
+// }
---
+++ reactjs/exercises/2/final2.js
@@ -0,0 +1,32 @@
+const header = ReactDOM.createRoot(document.getElementById('header'))  
+const main = ReactDOM.createRoot(document.getElementById('main'))
+
+header.render(
+    <HeaderContent />
+ )
+ 
+ main.render(
+    <MainContent />
+)
+
+function HeaderContent () {
+    return (
+       <header><h1>Corporate strategy</h1></header>
+    )
+}
+
+function MainContent () {
+    return (
+        <div>
+        <ImgContent />
+       <main><section><p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p></section></main>
+       </div>  
+    )
+}
+
+function ImgContent () {
+    return (
+        <figure><img src="https://lunevedy.com/ui/assets/img/1024x618-cafe.jpg" alt="sample image"/></figure>
+    )
+}
+
