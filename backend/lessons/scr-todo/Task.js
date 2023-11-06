1ff9d737aa594acf8bcf00ee6a7e93affa7b6413
Brendan Munnelly <munnellyb@gmail.com>
2023-11-06 03:49:54 +0000

    Add files via upload

--- backend/lessons/scr-todo/Task.js
+++ backend/lessons/scr-todo/Task.js
@@ -3,7 +3,7 @@ export const Task = (props) => {
     return (
         <div 
         className="Task"
-        style={ {backgroundColor: props.completed ? "green" : "white" } }
+        style={ {backgroundColor: props.completed ? "transparent" : "pink" } }
         ><h1>{props.taskName}</h1>
 
         <button onClick={() => props.completeTask(props.id)}> Complete </button>
