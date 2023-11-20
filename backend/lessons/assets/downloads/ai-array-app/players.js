8f0d73bab907562cb47caa8d89179d5ef639dc39
Brendan Munnelly <munnellyb@gmail.com>
2023-11-20 03:42:29 +0000

    Add files via upload

---
+++ backend/lessons/assets/downloads/ai-array-app/players.js
@@ -0,0 +1,121 @@
+export const players = [
+    {
+        id: 1,
+        firstname: "Lionel",
+        lastname: "Messi",
+        age: 34,
+        club: "Paris Saint-Germain",
+        trophies: 35,
+        nationality: "Argentinian",
+        bio: "Lionel Messi, born on June 24, 1987, in Rosario, Argentina, is widely regarded as one of the greatest football players of all time. He spent the majority of his career at Barcelona, where he won numerous titles, including four UEFA Champions League titles and ten La Liga titles. Messi is known for his incredible dribbling skills, vision, and scoring ability, having won the Ballon d'Or award multiple times. In August 2021, he transferred to Paris Saint-Germain after financial complications at Barcelona. Messi has also had a significant international career, representing Argentina in multiple World Cups and Copa America tournaments, leading them to victory in the 2021 Copa America. His legacy in football is marked by his breathtaking skill, humble personality, and numerous personal and team accolades.",
+        img: "https://en.wikipedia.org/wiki/File:Lionel_Messi_20180626.jpg" // This is a placeholder link
+    },
+    {
+        id: 2,
+        firstname: "Cristiano",
+        lastname: "Ronaldo",
+        age: 36,
+        club: "Manchester United",
+        trophies: 32,
+        nationality: "Portuguese",
+        bio: "Cristiano Ronaldo, born on February 5, 1985, in Funchal, Madeira, Portugal, is one of the most prolific forwards in football history. He has played for several top clubs including Sporting Lisbon, Manchester United, Real Madrid, and Juventus. Ronaldo is known for his incredible athleticism, goal-scoring record, and versatility on the field. Throughout his career, he has won five UEFA Champions League titles and numerous league titles in England, Spain, and Italy. Ronaldo is also a five-time Ballon d'Or winner and has numerous individual records to his name. Beyond his club success, he has been a key player for the Portugal national team, leading them to victory in the 2016 UEFA European Championship and the 2019 UEFA Nations League. Ronaldo's dedication, work ethic, and ability to perform in crucial moments have cemented his status as a football legend.",
+        img: "https://en.wikipedia.org/wiki/File:Cristiano_Ronaldo_2018.jpg" // This is a placeholder link
+    },
+
+    {
+        id: 3,
+        firstname: "Neymar",
+        lastname: "Jr",
+        age: 29,
+        club: "Paris Saint-Germain",
+        trophies: 23,
+        nationality: "Brazilian",
+        bio: "Neymar Jr, born on February 5, 1992, in Mogi das Cruzes, Brazil, is known for his flair, creativity, and skill on the ball. Starting his career at Santos in Brazil, Neymar quickly became a global sensation due to his remarkable talent. He moved to Barcelona in 2013, forming a formidable trio with Lionel Messi and Luis Suarez, and won numerous titles including the UEFA Champions League. In 2017, he made a record-breaking transfer to Paris Saint-Germain, where he continued to display his extraordinary skills. Neymar has been a pivotal player for the Brazil national team, contributing significantly in various international tournaments. His playstyle, marked by agility, quick thinking, and exceptional technical skills, makes him one of the most entertaining footballers to watch.",
+        img: "https://en.wikipedia.org/wiki/File:Neymar_Jr._2017.jpg" // Placeholder link
+    },
+    {
+        id: 4,
+        firstname: "Kylian",
+        lastname: "Mbappe",
+        age: 22,
+        club: "Paris Saint-Germain",
+        trophies: 10,
+        nationality: "French",
+        bio: "Kylian Mbappe, born on December 20, 1998, in Paris, France, is a rising star in world football, known for his speed, finishing, and composure. Mbappe first gained major attention at AS Monaco, helping them secure the Ligue 1 title. His rapid rise continued following his move to Paris Saint-Germain, where he has become a key player. Mbappe played a crucial role in France’s 2018 World Cup victory, becoming the youngest French player to score in a World Cup and the second teenager, after Pelé, to score in a World Cup Final. His impact on the field is significant, with an ability to change the course of a game single-handedly. Mbappe's career is still in its early stages, but he has already established himself as one of the world’s elite forwards.",
+        img: "https://en.wikipedia.org/wiki/File:Kylian_Mbappé_2018.jpg" // Placeholder link
+    },
+
+    {
+        id: 5,
+        firstname: "Robert",
+        lastname: "Lewandowski",
+        age: 32,
+        club: "Bayern Munich",
+        trophies: 25,
+        nationality: "Polish",
+        bio: "Robert Lewandowski, born on August 21, 1988, in Warsaw, Poland, is renowned for his positional sense, technique, and finishing. He is considered one of the best strikers in the world. Starting his career in Poland, Lewandowski moved to Borussia Dortmund in 2010, where he emerged as a top-class player. His transfer to Bayern Munich in 2014 further elevated his career. At Bayern, he has won numerous Bundesliga titles and the UEFA Champions League. Lewandowski has a record of scoring in consecutive Bundesliga games and is known for his ability to find the back of the net with remarkable consistency. On the international stage, he has been the backbone of the Polish national team, providing leadership and scoring prowess.",
+        img: "https://en.wikipedia.org/wiki/File:Robert_Lewandowski_2021.jpg" // Placeholder link
+    },
+
+    {
+        id: 6,
+        firstname: "Kevin",
+        lastname: "De Bruyne",
+        age: 29,
+        club: "Manchester City",
+        trophies: 9,
+        nationality: "Belgian",        
+        bio: "Kevin De Bruyne, born on June 28, 1991, in Drongen, Belgium, is widely regarded as one of the best midfielders in the world. His exceptional vision, passing, and shooting abilities make him a critical player for Manchester City and the Belgium national team. De Bruyne began his professional career at Genk, then moved to Chelsea, though he did not find much success there. His career took off after joining Wolfsburg, where his performances earned him a move to Manchester City in 2015. At City, De Bruyne has been instrumental in their domestic and international success, including multiple Premier League titles. Internationally, he has been a key player for Belgium, helping them achieve a third-place finish in the 2018 FIFA World Cup.",
+        img: "https://en.wikipedia.org/wiki/File:Kevin_De_Bruyne_2018.jpg" // Placeholder link
+    },
+
+    {
+        id: 7,
+        firstname: "Virgil",
+        lastname: "van Dijk",
+        age: 30,
+        club: "Liverpool",
+        trophies: 8,
+        nationality: "Dutch",     
+        bio: "Virgil van Dijk, born on July 8, 1991, in Breda, Netherlands, is considered one of the best defenders in the world. Known for his strength, leadership, and aerial ability, van Dijk has been a key player for Liverpool since his transfer from Southampton in 2018. He played a pivotal role in Liverpool's UEFA Champions League victory in 2019 and their first Premier League title in 30 years in 2020. Van Dijk's presence in defense has been a cornerstone of Liverpool’s success, earning him numerous individual accolades. Internationally, he captains the Netherlands national team and has been integral in their defense, known for his composure and tactical intelligence.",
+        img: "https://en.wikipedia.org/wiki/File:Virgil_van_Dijk_2019.jpg" // Placeholder link
+    },
+    {
+        id: 8,
+        firstname: "Sadio",
+        lastname: "Mane",
+        age: 29,
+        club: "Liverpool",
+        trophies: 7,
+        nationality: "Senegalese",           
+        bio: "Sadio Mane, born on April 10, 1992, in Sedhiou, Senegal, is renowned for his pace, finishing, and agility. Mane began his professional career at Metz in France before moving to Red Bull Salzburg, where his performances attracted the attention of Premier League club Southampton. His move to Liverpool in 2016 marked the beginning of a highly successful period, contributing significantly to their Champions League and Premier League triumphs. Mane's playing style, characterized by his speed and ability to beat defenders, has made him one of the top forwards in the world. For Senegal, he has been a key player, leading them in multiple African Cup of Nations tournaments and the FIFA World Cup.",
+        img: "https://en.wikipedia.org/wiki/File:Sadio_Mane_2019.jpg" // Placeholder link
+    },
+
+    {
+        id: 9,
+        firstname: "Mohamed",
+        lastname: "Salah",
+        age: 29,
+        club: "Liverpool",
+        trophies: 6,
+        nationality: "Egyptian",
+        bio: "Mohamed Salah, born on June 15, 1992, in Nagrig, Egypt, is celebrated for his speed, dribbling, and finishing. Salah's career took off after he moved to Liverpool from AS Roma in 2017. At Liverpool, he has been a phenomenal forward, breaking the record for the most goals in a 38-game Premier League season. His contributions have been crucial in Liverpool’s recent successes, including the UEFA Champions League and the Premier League. Salah's impact extends beyond the club level; he is a national hero in Egypt, leading their national team in the World Cup and the African Cup of Nations. His playing style, marked by his incredible pace and ability to find the back of the net consistently, has made him one of the most formidable attackers in the game.",
+        img: "https://en.wikipedia.org/wiki/File:Mohamed_Salah_2018.jpg" // Placeholder link
+    },
+
+    {
+        id: 10,
+        firstname: "Sergio",
+        lastname: "Ramos",
+        age: 35,
+        club: "Paris Saint-Germain",
+        trophies: 22,
+        nationality: "Spanish",        
+        bio: "Sergio Ramos, born on March 30, 1986, in Camas, Spain, is one of the most decorated defenders of his generation. Known for his aggressive play, leadership, and goal-scoring ability, Ramos has had a storied career with Real Madrid and the Spanish national team. At Real Madrid, he won numerous titles, including four UEFA Champions League trophies. Ramos is also celebrated for his critical goals in key matches, notably his equalizing goal in the 2014 Champions League final. Internationally, he has been a part of Spain's golden generation, winning the 2010 FIFA World Cup and two European Championships. His move to Paris Saint-Germain in 2021 marked a new chapter in his illustrious career.",
+        img: "https://en.wikipedia.org/wiki/File:Sergio_Ramos_2018.jpg" // Placeholder link
+    },
+
+];
+
+
