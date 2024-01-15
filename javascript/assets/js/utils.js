9252659f2678733b6a67b1dc7bd7b1aa730e73b5
Brendan Munnelly <munnellyb@gmail.com>
2024-01-15 02:56:38 +0000

    Add files via upload

--- javascript/assets/js/utils.js
+++ javascript/assets/js/utils.js
@@ -1,7 +1,5 @@
 
-// Copy code sample to clipboard
-
-    const btnsJS = document.querySelectorAll('main .copy-code');
+    const btnsJS = document.querySelectorAll('main article .copy-code');
     const btnsArrJS = Array.from(btnsJS);
     let btnJSid;
 
@@ -16,22 +14,6 @@
         });
     });
 
-    const btnsHTML = document.querySelectorAll('main .copy-html');
-    const btnsArrHTML = Array.from(btnsHTML);
-    let btnHTMLID;
-
-    btnsArrHTML.forEach(btnHTML => {
-        btnHTML.addEventListener("click", e => {
-            e.stopPropagation();
-            btnHTMLID = e.target.id;
-            console.log("You clicked button: "+btnHTMLID);
-            // remove 'btn-' from ID
-            btnHTMLID = btnHTMLID.replace('btn-', '');
-            doCopyHTML(btnHTMLID);
-        });
-    });
-
-
     async function doCopyCode(id) {
         const strCode = document.getElementById('strCode-'+id).textContent;
         await navigator.clipboard.writeText(strCode);
@@ -39,24 +21,10 @@
         document.getElementById("btn-"+id).style.borderColor= "#00ff00";
         document.getElementById("btn-"+id).innerHTML = "Copied <i class=\"fa-solid fa-check\"><\/i>";
         setTimeout(function() {
-            document.getElementById("btn-"+id).style.color= "";
-            document.getElementById("btn-"+id).style.borderColor= "";
-            document.getElementById("btn-"+id).innerHTML = "Copy Code <i class=\"fa-brands fa-square-js\"><\/i>";
+            document.getElementById("btn-"+id).style.color= "lightskyblue";
+            document.getElementById("btn-"+id).style.borderColor= "lightskyblue";
+            document.getElementById("btn-"+id).innerHTML = "Copy <i class=\"fa-regular fa-copy\"><\/i>";
             document.getElementById("btn-"+id).classList.add('copy-code');	
-        }, 1500);
-    }
-
-    async function doCopyHTML(id) {
-        const strCode = document.getElementById('strCode-'+id).textContent;
-        await navigator.clipboard.writeText(strCode);
-        document.getElementById("btn-"+id).style.color= "#00ff00";
-        document.getElementById("btn-"+id).style.borderColor= "#00ff00";
-        document.getElementById("btn-"+id).innerHTML = "Copied <i class=\"fa-solid fa-check\"><\/i>";
-        setTimeout(function() {
-            document.getElementById("btn-"+id).style.color= "";
-            document.getElementById("btn-"+id).style.borderColor= "";
-            document.getElementById("btn-"+id).innerHTML = "Copy HTML <i class=\"fa-solid fa-code\"><\/i>";
-            document.getElementById("btn-"+id).classList.add('copy-html');	
         }, 1500);
     }
 
