59a3a5de104edf9927c3b1e8b285e371e8cb4cc2
Brendan Munnelly <munnellyb@gmail.com>
2024-02-26 23:57:32 +0000

    Add files via upload

--- full-stack/express-mongodb/assets/downloads/jwts/usersControllers.js
+++ full-stack/express-mongodb/assets/downloads/jwts/usersControllers.js
@@ -1,10 +1,8 @@
 import asyncHandler from 'express-async-handler';
 import UserModel from '../models/userModel.js';
-import dotenv from 'dotenv'
-dotenv.config()
 import jwt from 'jsonwebtoken';
-const secret = process.env.JWT_SECRET;
 import bcrypt from 'bcrypt';
+const secret = process.env.JWT_SECRET; // Import secret from .env file
 
 // Sign up user
 const user_add = asyncHandler(async (req, res) => {
@@ -20,31 +18,31 @@ const user_add = asyncHandler(async (req, res) => {
         res.status(201).json(savedUser);
     } catch (error) {
         res.status(500).json({ message: error.message });
-    }
-});
+    }});
 
 // Login user
 const user_login = asyncHandler(async (req, res) => {
-    console.log("hello user login");
+    console.log("hello user login")
     try {
         const user = await UserModel.findOne({ email: req.body.email });
+        console.log("user with email", req.body.email, "does not exist")
+
         if (!user) {
             return res.status(400).json({ message: 'Cannot find user' });
         }
+        console.log("user with email", req.body.email, "exists")
+
         if (await bcrypt.compare(req.body.password, user.password)) {
-            console.log(`Secret: ${secret}`); // Log the secret
-            console.log(`User ID: ${user._id}`); // Log the user ID
             // User's email and password are correct
             // Generate a token for the user
             const token = jwt.sign({ id: user._id }, secret, { expiresIn: '1h' });
             // Send the token back to the client
-            res.cookie('token', token, { httpOnly: true, maxAge: 3600000 });     
-            res.json({ message: 'Success', token: token });   
-           
+            // res.json({ message: 'Success', token: token });        
         } else {
             res.status(403).json({ message: 'Not Allowed' }); 
         }
     } catch (error) {
+        console.error(error); // Log the error stack trace
         res.status(500).json({ message: error.message });
     }
 });
