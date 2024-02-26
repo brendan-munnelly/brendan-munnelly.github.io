f2bee948c12c17ed2084213a4d89f5b5ce02ac6c
Brendan Munnelly <munnellyb@gmail.com>
2024-02-26 21:29:18 +0000

    Add files via upload

--- full-stack/express-mongodb/assets/downloads/jwts/playersRoutes.js
+++ full-stack/express-mongodb/assets/downloads/jwts/playersRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express'
 const router = express.Router();
 import { player_add, player_delete, player_get, player_update, players_index } from '../controllers/playersControllers.js';;
-import { verifyToken } from '../app.js';
 
 // Home page with link
 router.get('/', async (req, res) => {
@@ -15,12 +14,12 @@ router.get('/soccer_players/list', players_index);
 router.get('/soccer_players/view/:id', player_get);
 
 // Add a new player
-router.post('/soccer_players/add', verifyToken, player_add);
+router.post('/soccer_players/add', player_add);
 
 // Update a player selected by id 
-router.put('/soccer_players/update/:id', verifyToken, player_update);
+router.put('/soccer_players/update/:id', player_update);
 
 // Delete a player selected by id
-router.delete('/soccer_players/delete/:id', verifyToken, player_delete);
+router.delete('/soccer_players/delete/:id', player_delete);
 
 export default router;
