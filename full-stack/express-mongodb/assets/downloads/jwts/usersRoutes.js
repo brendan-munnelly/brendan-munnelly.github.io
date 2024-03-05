8ac3c8d2694fe591eef56caf970f87d0a0eea842
Brendan Munnelly <munnellyb@gmail.com>
2024-03-05 18:52:19 +0000

    Add files via upload

--- full-stack/express-mongodb/assets/downloads/jwts/usersRoutes.js
+++ full-stack/express-mongodb/assets/downloads/jwts/usersRoutes.js
@@ -8,9 +8,4 @@ router.post('/soccer_players/users/add', user_add);
 // login user
 router.post('/soccer_players/users/login', user_login);
 
-// access denied page
-router.get('/', async (req, res) => {
-    res.send('access_denied.html');
-});
-
 export default router;
