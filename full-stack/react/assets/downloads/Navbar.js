d6f244a29effd2ac3797b7a238674673b2a91a86
Brendan Munnelly <munnellyb@gmail.com>
2024-03-12 08:45:43 +0000

    Add files via upload

---
+++ full-stack/react/assets/downloads/Navbar.js
@@ -0,0 +1,16 @@
+// Navbar.js
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+          <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/products">Products</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+          </ul>
+  </nav>
+  );
+}
+
+export default Navbar;
