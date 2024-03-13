643f34925d9329f495b9f3563db00b893c4735f8
Brendan Munnelly <munnellyb@gmail.com>
2024-03-13 08:05:04 +0000

    Add files via upload

---
+++ full-stack/react/assets/downloads/Products.jsx
@@ -0,0 +1,26 @@
+export const Products = () => {
+    return (
+        <>
+
+        <header className="theme-light text-center-desktop text-center-mobile">
+
+            <div className="container-text">
+                <h1>Products page</h1>
+            </div>
+
+        </header>
+
+            <section className="w-820px">
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium animi porro excepturi quam est iure enim, veniam repudiandae doloribus nemo eius necessitatibus ex architecto eligendi dignissimos nobis et minima tenetur?</p>
+
+                <p>This is a link to the <a href="./">Home</a> page.</p>
+                <p>This is a link to the <a href="./contact">Contact</a> page.</p>
+
+            </section>
+        </>
+    );
+}
+
+
+export default Products;
