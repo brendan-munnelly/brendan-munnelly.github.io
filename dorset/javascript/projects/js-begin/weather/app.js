fa37b61b6af3029afd580e4117b174fd52d1379a
Brendan Munnelly <munnellyb@gmail.com>
2025-03-21 15:09:41 +0000

    Add files via upload

---
+++ dorset/javascript/projects/js-begin/weather/app.js
@@ -0,0 +1,45 @@
+if (document.readyState === "loading") {
+    // Loading hasn't finished yet
+    document.addEventListener("DOMContentLoaded", populateTableRows);
+} else {
+    // DOMContentLoaded has already fired
+    populateTableRows();
+}
+
+capitalize = s => s && s[0].toUpperCase() + s.slice(1);
+
+async function populateTableRows() { 
+    await fetch('https://api.openweathermap.org/data/2.5/weather?q=dublin,ie&units=metric&APPID=bc425ac2188d406c884f4fdd88b339f0')
+
+    .then(response => {
+        if (response.status !== 200) {
+            console.log('Error Status Code: ' + response.status);
+            return;
+        }
+        response.json().then((data) => {
+            console.log(data);
+                let strTableRows = `<tr>
+                <td><span>Summary</span></td>
+                <td>${capitalize(data["weather"][0]["description"])}</td>
+                </tr>
+                    <tr>
+                        <td><span>Temperature</span></td>
+                        <td>${data["main"]["temp"] + " &deg;C"}</td>
+                    </tr>
+                <tr>
+                    <td><span>Humidity</span></td>
+                    <td>${data["main"]["humidity"] + " %"}</td>
+                </tr>
+                <tr>
+                    <td><span>Pressure</span></td>
+                    <td>${data["main"]["pressure"] + " Pa"}</td>
+                </tr>`;
+                document.querySelector("#table-weather-dublin tbody").innerHTML = strTableRows;
+            });
+           
+        })
+
+        .catch(error => {
+            // handle any error
+       });
+}
