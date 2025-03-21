c5b723f5824d9d7fb352b91747aeffa40e7c4cf9
Brendan Munnelly <munnellyb@gmail.com>
2025-03-21 15:03:13 +0000

    Add files via upload

---
+++ dorset/javascript/projects/projects-final/calculator-modules/perform-calculations.js
@@ -0,0 +1,13 @@
+export function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    }
+  
+    return secondOperand;
+  }
